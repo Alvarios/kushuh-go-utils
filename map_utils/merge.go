d5ae0bfbcc4ccae26edfa8972cb5e4e2c32b32bb
Kushuh <kuzanisu@gmail.com>
2020-05-31 13:39:46 +0200

    updated arguments

--- map_utils/merge.go
+++ map_utils/merge.go
@@ -1,21 +1,38 @@
 package map_utils
 
-func Merge(origin map[string]interface{}, invaders ...map[string]interface{}) map[string]interface{} {
+import "errors"
+
+func Merge(origin interface{}, invaders ...interface{}) (interface{}, error) {
+	mo, ok := origin.(map[string]interface{})
+
+	if ok == false {
+		return nil, errors.New("cannot convert first argument to map object")
+	}
+
 	for _, element := range invaders {
-		for key, value := range element {
+		me, ok := element.(map[string]interface{})
+		if ok == false {
+			return nil, errors.New("argument is not of type map[string]interface{}")
+		}
+
+		for key, value := range me {
 			mappedValue, ok := value.(map[string]interface{})
-			mappedOriginValue, originOk := origin[key].(map[string]interface{})
+			mappedOriginValue, originOk := mo[key].(map[string]interface{})
 
 			if ok {
 				if originOk == false {
 					mappedOriginValue = map[string]interface{}{}
 				}
-				origin[key] = Merge(mappedOriginValue, mappedValue)
+				newValue, err := Merge(mappedOriginValue, mappedValue)
+				if err != nil {
+					return nil, err
+				}
+				mo[key] = newValue
 			} else {
-				origin[key] = value
+				mo[key] = value
 			}
 		}
 	}
 
-	return origin
+	return mo, nil
 }
