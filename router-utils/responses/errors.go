084da02e3552dcfab721d7ecea75f403d33000b0
Kushuh <kuzanisu@gmail.com>
2020-06-14 15:50:18 +0200

    updated error object with Error() method

--- router-utils/responses/errors.go
+++ router-utils/responses/errors.go
@@ -1,6 +1,12 @@
 package responses
 
+import "fmt"
+
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("code %v : %s", e.Code, e.Message)
 }
