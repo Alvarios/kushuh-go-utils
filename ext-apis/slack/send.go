b3f7b80b439ecd363ba9f9c04cee324cd432a139
Kushuh <kuzanisu@gmail.com>
2020-07-19 19:42:06 +0200

    added attachments and blocks for Slack API

--- ext-apis/slack/send.go
+++ ext-apis/slack/send.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
-func Send(w string, m string) (*http.Response, error) {
-	return httpUtils.Post(w, map[string]interface{}{"text" : m})
+func Send(w string, m string, b map[string]interface{}, a []map[string]interface{}) (*http.Response, error) {
+	fm := map[string]interface{}{"text" : m}
+
+	if b != nil {
+		fm["blocks"] = b
+	}
+
+	if a != nil {
+		fm["attachments"] = a
+	}
+
+	return httpUtils.Post(w, fm)
 }
--- ext-logs/errors.go
+++ ext-logs/errors.go
@@ -13,10 +13,9 @@ type ServerConfig struct {
 	Environment string
 }
 
-func (s *ServerConfig) Print(t string) string {
+func (s *ServerConfig) Print() string {
 	return fmt.Sprintf(
-		"*%s*\n>_in %s_\n>*Environment*\n>%s\n>*Noticed*\n>%s",
-		t,
+		"_in %s_\n*Environment*\n%s\n*Noticed*\n%s",
 		s.Application,
 		s.Environment,
 		time.Now().Format("2006-01-02 3:4:5"),
@@ -24,19 +23,73 @@ func (s *ServerConfig) Print(t string) string {
 }
 
 func (s *ServerConfig) Error(t string) {
-	_, _ = slack.Send(s.Webhook, s.Print(t))
+	_, _ = slack.Send(
+		s.Webhook,
+		"*Unexpected error.*",
+		nil,
+		[]map[string]interface{}{
+			{
+				"fallback" : fmt.Sprintf("Unexpected error in %s (%s)", s.Application, s.Environment),
+				"color" : "#FF9300",
+				"author_name" : s.Application,
+				"title" : t,
+				"text" : s.Print(),
+			},
+		},
+	)
 }
 
 func (s *ServerConfig) Errorf(t string, par ...interface{}) {
-	_, _ = slack.Send(s.Webhook, s.Print(fmt.Sprintf(t, par...)))
+	_, _ = slack.Send(
+		s.Webhook,
+		"*Unexpected error.*",
+		nil,
+		[]map[string]interface{}{
+			{
+				"fallback" : fmt.Sprintf("Unexpected error in %s (%s)", s.Application, s.Environment),
+				"color" : "#FF9300",
+				"author_name" : s.Application,
+				"title" : fmt.Sprintf(t, par...),
+				"text" : s.Print(),
+			},
+		},
+	)
 }
 
 func (s *ServerConfig) Fatal(t string) {
-	_, _ = slack.Send(s.Webhook, s.Print(t))
+	_, _ = slack.Send(
+		s.Webhook,
+		"*Unexpected error.*",
+		nil,
+		[]map[string]interface{}{
+			{
+				"fallback" : fmt.Sprintf("Unexpected error in %s (%s)", s.Application, s.Environment),
+				"color" : "#FF9300",
+				"author_name" : s.Application,
+				"title" : t,
+				"text" : s.Print(),
+			},
+		},
+	)
+
 	log.Fatal(t)
 }
 
 func (s *ServerConfig) Fatalf(t string, par ...interface{}) {
-	_, _ = slack.Send(s.Webhook, s.Print(fmt.Sprintf(t, par...)))
+	_, _ = slack.Send(
+		s.Webhook,
+		"*Unexpected error.*",
+		nil,
+		[]map[string]interface{}{
+			{
+				"fallback" : fmt.Sprintf("Unexpected error in %s (%s)", s.Application, s.Environment),
+				"color" : "#FF9300",
+				"author_name" : s.Application,
+				"title" : fmt.Sprintf(t, par...),
+				"text" : s.Print(),
+			},
+		},
+	)
+
 	log.Fatalf(t, par...)
 }
